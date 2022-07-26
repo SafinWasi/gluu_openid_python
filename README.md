47eee1730adbe1dd4b4162b27da5096cb362b7f4
SafinWasi <safinwasi_11@outlook.com>
2022-07-25 19:55:14 -0500

    Finishing

--- README.md
+++ README.md
@@ -1,10 +1,11 @@
 # gluu_openid_python
 
-This python script will allow you to use dynamic client registration to register a client with your Gluu server, optionally using a [JSON Web Key Set](https://datatracker.ietf.org/doc/html/rfc7517#section-5) file to allow for JWK authentication via OpenID. 
+This python script will allow you to use [Dynamic Client Registration](https://openid.net/specs/openid-connect-registration-1_0.html) to register a client with your Gluu server, optionally using a [JSON Web Key Set](https://datatracker.ietf.org/doc/html/rfc7517#section-5) file to allow for JWK authentication via OpenID. 
 
 ## Requirements
 - Python3
 - pip3
+- The SSL certificate from your Gluu server, which can be found at `Configuration` > `Certificates` > `HTTPD SSL` and clicking `Download`. The script expects `httpd.crt` by default. 
 - (Optional) A JWKS file containing a JSON Web Key Set. The script expects `client-key.jwks` by default.
 
 ## Setup
@@ -12,4 +13,13 @@ This python script will allow you to use dynamic client registration to register
 2. Set up a virtual python environment: `python -m venv venv`
 3. And activate it: `source venv/bin/activate`
 4. Install requirements: `pip install -r requirements.txt`
-5. Run the script: `python scim_client`
+5. Run the script: `python gluu_client_reg.py <hostname> <path to host's SSL certificate> <path to your Java Keystore file>`
+6. Enter a name for your new client when prompted. Remember this; you will need it later.
+7. If the script runs successfully, the new client ID and secret will be saved to `client.json`. **Do not** share this information.
+
+## Notes
+The script is meant to be run directly using Python; however, you may use it as a module in your own programs if you wish.
+
+## References
+- [Gluu 4.4 Documentation](https://gluu.org/docs/gluu-server/4.4/)
+- [JWK Specification](https://datatracker.ietf.org/doc/html/rfc7517)
--- gluu_client_reg.py
+++ gluu_client_reg.py
@@ -3,81 +3,94 @@ import sys
 import requests
 
 
-class ScimClient:
+class GluuClientReg:
     """An implementation of a simple client application to create an OpenID user on Gluu."""
 
-    def __init__(self, hostName, certFile):
-        self.HOSTNAME = hostName
-        self.CERT = certFile
-        self.JWKS_ENDPOINT = hostName + "oxauth/restv1/jwks"
-        self.TOKEN_ENDPOINT = None
-        self.OPENID_CONFIGS = hostName + ".well-known/openid-configuration"
+    def __init__(self, host_name:str, cert_file:str):
+        """
+        Constructor for GluuClientReg.
+        host_name: The hostname of the Gluu server.
+        cert_file: The SSL certificate file of the Gluu server.
+        """
+        if host_name[-1] != "/":
+            host_name += "/"
+        self.host_name = host_name
+        self.cert = cert_file
+        self.token_endpoint = None
+        self.openid_configs = host_name + ".well-known/openid-configuration"
 
         self.jwks = None
         self.client_id = None
         self.client_secret = None
 
-    def get_jwks(self, file="client-key.jwks"):
-        """Reads a JWKS file and loads the JSON web key set."""
-        with open(file, 'r') as f:
-            data = json.loads(f.read())
+    def get_jwks(self, file_name:str="client-key.jwks"):
+        """
+        Reads a JWKS file and loads the JSON web key set.
+        file_name: The JWKS file containing your JSON Web Key Set. Defaults to "client-key.jwks"
+        """
+        with open(file_name, "r") as file:
+            data = json.loads(file.read())
         self.jwks = data
 
     def get_client(self):
         """
         Sends a POST request to the server for dynamic client registration.
         The client will optionally use private_key_jwt for authentication if
-        ScimClient.get_jwks() was called.
+        ScimClient.get_jwks() was called. Otherwise, client_secret_basic will 
+        be used.
         """
-        r = requests.get(self.OPENID_CONFIGS, verify=self.CERT)
+        r = requests.get(self.openid_configs, verify=self.cert)
         if r.status_code != 200:
             print("Request returned", r.status_code)
         else:
             data = r.json()
             reg = data["registration_endpoint"]
-            self.TOKEN_ENDPOINT = data["token_endpoint"]
+            self.token_endpoint = data["token_endpoint"]
             print("Enter a name for the new client: ", end="")
             client_name = input()
             payload = {
                 "application_type": "native",
-                "redirect_uris": [self.HOSTNAME + "callback"],
+                "redirect_uris": [self.host_name + "callback"],
                 "client_name": client_name,
-                "grant_types": ["client_credentials"]
+                "grant_types": ["client_credentials"],
             }
             if self.jwks is not None:
-                payload['jwks'] = self.jwks
+                payload["jwks"] = self.jwks
                 payload["token_endpoint_auth_method"] = "private_key_jwt"
 
-            
-            r2 = requests.post(reg, json=payload, verify=self.CERT)
+            r2 = requests.post(reg, json=payload, verify=self.cert)
             if r2.status_code == 200:
                 client_id = r2.json()["client_id"]
                 client_secret = r2.json()["client_secret"]
                 output = {"client_id": client_id, "client_secret": client_secret}
                 print("Writing client ID and secret to client.json...")
-                with open("client.json", "w") as f:
-                    f.write(json.dumps(output, indent=4))
-                print(json.dumps(r2.json(), indent=4))
+                with open("client.json", "w") as file:
+                    file.write(json.dumps(output, indent=4))
+                # print(json.dumps(r2.json(), indent=4))
+                print("Write successful.")
             else:
                 print("Returned status code", r2.status_code)
                 print(r2.content)
 
-    def read_client(self, file="client.json"):
-        """Reads an existing JSON file for client configuration."""
-        with open(file, "r") as f:
-            data = json.loads(f.read())
+    def read_client(self, file_name:str="client.json"):
+        """
+        Reads an existing JSON file for client configuration.
+        file_name: JSON file containing client_id and client_secret. Defaults to "client.json".
+        """
+        with open(file_name, "r") as file:
+            data = json.loads(file.read())
         self.client_id = data["client_id"]
         self.client_secret = data["client_secret"]
 
 
-if len(sys.argv) < 2:
-    print("Usage: python scim_client.py <hostname> <path to host's cacert> <path to your Java Ketstore file>")
-    exit()
+if __name__ == "__main__":
+    if len(sys.argv) < 3:
+        print(
+            "Usage: python gluu_client_reg.py <host_name> <path to host's SSL certificate> <path to your Java Keystore file>"
+        )
+        sys.exit()
 
-
-
-testClient = ScimClient(
-    sys.argv[1], sys.argv[2]
-)
-testClient.get_jwks(sys.argv[3])
-testClient.get_client()
+    testClient = GluuClientReg(sys.argv[1], sys.argv[2])
+    if len(sys.argv) == 4:
+        testClient.get_jwks(sys.argv[3])
+    testClient.get_client()
